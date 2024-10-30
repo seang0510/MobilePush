90485f722fd78aa58c93cf64177aadbb0ca7480e
seang0510 <59646825+seang0510@users.noreply.github.com>
2024-10-30 15:11:34 +0900

    444

---
+++ files/app.js
@@ -0,0 +1,8 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const skylog = require("./helper/logger");
+
+console.log("test");
