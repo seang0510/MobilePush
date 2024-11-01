62fc16ae77b038941ad0bf883862f5721e38e65d
seang0510 <59646825+seang0510@users.noreply.github.com>
2024-11-01 10:05:18 +0900

    # push array 처리

--- controller/index.js
+++ controller/index.js
@@ -13,11 +13,16 @@ fcm.initializeApp({
 exports.coadmasterMobilePush = async (req, res, next) => {
 
     let resModel;
-    const token = req.body.token;
+    const tokenList = req.body.tokens;
     const title = req.body.title;
     const body = req.body.body;
     const url = req.body.linkUrl;
-    console.log('coadmasterMobilePush token : ' + token);
+    skylog.info('coadmasterMobilePush tokenList : ' + tokenList);
+
+  //   const tokenList1 = [
+  //     'cwL1ayQxS0MSjUW2rDJuld:APA91bGgShnvv4o83A4oFYCqyql1wIMyBe7_sfbGU9JwGoX2uIMmA7LGWOq5caXyY1yxfRrUhJ23YaoZ9UcqWI9DexO9ikJBax3uL4JZrU03lDaJVX0GvDA',
+  //     'cwL1ayQxS0MSjUW2rDJuld:APA91bGgShnvv4o83A4oFYCqyql1wIMyBe7_sfbGU9JwGoX2uIMmA7LGWOq5caXyY1yxfRrUhJ23YaoZ9UcqWI9DexO9ikJBax3uL4JZrU03lDaJVX0GvDA'
+  //  ];
 
 
     let message = {
@@ -30,13 +35,13 @@ exports.coadmasterMobilePush = async (req, res, next) => {
           message: body,
           linkUrl : url
         },
-        token: token,
+        tokens: tokenList,
       }
 
 
-    fcm.messaging().send(message).then(function (response) {
-      console.log('Successfully sent message: : ', response);
-      resModel = helper.createResponseModel(true , ""+response , token);
+    fcm.messaging().sendEachForMulticast(message).then(function (response) {
+      skylog.info('Successfully sent message: : ', response);
+      resModel = helper.createResponseModel(true , ""+response , tokenList);
       
       skylog.info("### AUTODOORA[200] ###"); 
       skylog.info("resModel :: " + JSON.stringify(resModel)); 
@@ -47,7 +52,7 @@ exports.coadmasterMobilePush = async (req, res, next) => {
     })
     .catch(function (err) {
       console.log('Error Sending message!!! : ', err);
-      resModel = helper.createResponseModel(false , ""+err , token);
+      resModel = helper.createResponseModel(false , ""+err , tokenList);
       skylog.info("### AUTODOORA[500] ###"); 
       skylog.info("resModel :: " + JSON.stringify(resModel)); 
       skylog.info("######################"); 
